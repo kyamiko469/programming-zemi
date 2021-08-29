0ae1bd285b0e74be4247aa4cf5539be7f40aa5a5
kyamiko469 <hoshi46ume373@gmail.com>
2021-08-29 05:54:36 +0000

    validation and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,9 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-	
-	def user_params
-		params.require(:user).permit(:name, :email)
+private
+
+  def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_081227) do
+ActiveRecord::Schema.define(version: 2021_08_29_041839) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
